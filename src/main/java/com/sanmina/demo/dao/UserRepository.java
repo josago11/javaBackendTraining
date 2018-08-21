b3b9b70a2110cd8e9e4b881953e2d90e2d654ef1
Gomez <santiago.gomez@sanmina.com>
2018-08-21 12:13:06 -0500

    Method getUsers created

--- src/main/java/com/sanmina/demo/dao/UserRepository.java
+++ src/main/java/com/sanmina/demo/dao/UserRepository.java
@@ -1,5 +1,7 @@
 package com.sanmina.demo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sanmina.demo.model.User;
@@ -7,4 +9,6 @@ import com.sanmina.demo.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 	@SuppressWarnings("unchecked")
 	User save(User user);
+	
+	//List<User> findAll();
 }
--- src/main/java/com/sanmina/demo/service/UserService.java
+++ src/main/java/com/sanmina/demo/service/UserService.java
@@ -1,9 +1,15 @@
 package com.sanmina.demo.service;
 
+import java.util.List;
+
 import com.sanmina.demo.model.User;
 
 public interface UserService {
 
 	User save(User user);
+
+	List<User> findAll();
+
+	
 
 }
