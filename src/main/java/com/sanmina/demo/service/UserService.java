72ef387f136760180890db994061535e4b836fc1
Gomez <santiago.gomez@sanmina.com>
2018-08-26 16:10:40 -0500

    Backend listo

--- src/main/java/com/sanmina/demo/service/UserService.java
+++ src/main/java/com/sanmina/demo/service/UserService.java
@@ -6,9 +6,18 @@ import com.sanmina.demo.model.User;
 
 public interface UserService {
 
+	/*
+	 * Guarda un usuario
+	 */
 	User save(User user);
-
+	/*
+	 * Recupera la lista de usuarios
+	 */
 	List<User> findAll();
+	/*
+	 * elimina un usuario
+	 */
+	void deleteUser(Long id);
 
 	
 
